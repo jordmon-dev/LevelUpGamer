22d7373d913e4ea3d9edc873e4b4f142d16ec275
Jordy <jord.mondaca@duocuc.cl>
2025-10-24 22:26:02 -0300

    Implementación de navegación a la pantalla del Home

--- app/src/main/java/com/example/levelup_gamer/Screen/Home.kt
+++ app/src/main/java/com/example/levelup_gamer/Screen/Home.kt
@@ -1,2 +1,34 @@
 package com.example.levelup_gamer.Screen
 
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.levelup_gamer.viewmodel.AppScreens
+
+
+@Composable
+
+fun Home(navController: NavController){
+    Column {
+        Text("Bienvenido a")
+        Spacer(modifier = Modifier.height(10.dp))
+        Text("Level-Up-Gamer")
+        Spacer(modifier = Modifier.height(10.dp))
+        Button(
+            onClick = {
+
+                navController.navigate(AppScreens.AboutScreen.route)
+
+            }
+        ) {
+            Text("Acerca de")
+        }
+    }
+}
