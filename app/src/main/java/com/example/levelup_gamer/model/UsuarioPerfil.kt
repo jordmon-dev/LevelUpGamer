e1c4ee235498233303a9efd02ab03c76738f0bd4
Jordy <jord.mondaca@duocuc.cl>
2025-10-31 20:01:06 -0300

    Se crean data class "Usuario Perfil" y "UsuarioErorres" en el archivo UsuarioPerfil.kt

---
+++ app/src/main/java/com/example/levelup_gamer/model/UsuarioPerfil.kt
@@ -0,0 +1,18 @@
+package com.example.levelup_gamer.model
+
+// clase con los atributos del usuario
+data class UsuarioPerfil(
+    val nombre: String="",
+    val correo: String="",
+    val password: String="",
+    val aceptarTerminos: Boolean = false,
+    val errores: UsuarioErrores = UsuarioErrores()
+)
+
+// almacena los errores de cada atributo
+data class UsuarioErrores(
+    val nombre: String? = null,
+    val correo: String? = null,
+    val password: String? = null,
+    val aceptaTerminos: String?=null
+)
