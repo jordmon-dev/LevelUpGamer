f29737cafe686b5bc9acdae69496eb641c8c5379
Jordy <jord.mondaca@duocuc.cl>
2025-10-31 19:51:17 -0300

    Se crea data class Producto en package modelo

---
+++ app/src/main/java/com/example/levelup_gamer/model/Producto.kt
@@ -0,0 +1,10 @@
+package com.example.levelup_gamer.model
+
+data class Producto(
+    val codigo: String,
+    val nombre: String,
+    val precio: Double,
+    val categoria: String,
+    val descripcion: String = ""
+)
+
