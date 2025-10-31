9f7b7baaec3638a3f7b31128ce96b5875292756e
Jordy <jord.mondaca@duocuc.cl>
2025-10-31 19:49:08 -0300

    Se crea la vista BienvenidaScreen.kt

---
+++ app/src/main/java/com/example/levelup_gamer/ui/theme/Screen/BienvenidaScreen.kt
@@ -0,0 +1,24 @@
+package com.example.levelup_gamer.ui.theme.Screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.levelup_gamer.viewmodel.UsuarioViewModel
+
+@Composable
+fun BienvenidaScreen(viewModel: UsuarioViewModel){
+
+    val usuario by viewModel.usuario.collectAsState();
+
+    Column(modifier = Modifier.padding(20.dp)) {
+        Text("Bienvenido",
+            style = MaterialTheme.typography.headlineLarge)
+        Text("Holi ${usuario.nombre}",
+            style = MaterialTheme.typography.bodyMedium)
+    }
+}
