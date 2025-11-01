83a48f08d795948837c06410f476ccaeea2466cc
Jordy <jord.mondaca@duocuc.cl>
2025-11-01 17:57:48 -0300

    Se modifica dependencia "icons-extended" a una versión más reciente

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,8 +48,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.6")
-    implementation("androidx.compose.material:material-icons-extended:1.6.8")
-    implementation("androidx.compose.material:material-icons-extended:1.6.8") // Usa la versión más reciente
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
     implementation("androidx.navigation:navigation-compose:2.8.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
