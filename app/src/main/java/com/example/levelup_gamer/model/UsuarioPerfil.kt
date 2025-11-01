1f2b2791e768c31ae867c4af2b7c217402a6abf7
Jordy <jord.mondaca@duocuc.cl>
2025-11-01 18:12:43 -0300

    Se agregan variables: edad, email, nivel, fechaRegistro, puntosLvelUp, confirmPassword, aceptarTerminos.

--- app/src/main/java/com/example/levelup_gamer/model/UsuarioPerfil.kt
+++ app/src/main/java/com/example/levelup_gamer/model/UsuarioPerfil.kt
@@ -1,12 +1,20 @@
 package com.example.levelup_gamer.model
 
+import android.provider.ContactsContract
+
 // clase con los atributos del usuario
 data class UsuarioPerfil(
     val nombre: String="",
     val correo: String="",
     val password: String="",
     val aceptarTerminos: Boolean = false,
-    val errores: UsuarioErrores = UsuarioErrores()
+    val errores: UsuarioErrores = UsuarioErrores(),
+    val edad: String="",
+    val email: String="",
+    val nivel: String="",
+    val fechaRegistro:String="",
+    val puntosLevelUp: Int=0,
+    val confirmPassword: String=""
 )
 
 // almacena los errores de cada atributo
@@ -14,5 +22,8 @@ data class UsuarioErrores(
     val nombre: String? = null,
     val correo: String? = null,
     val password: String? = null,
-    val aceptaTerminos: String?=null
-)
+    val aceptaTerminos: String? =null,
+    val edad: String? ="",
+    val confirmPassword: String? = null
+
+    )
