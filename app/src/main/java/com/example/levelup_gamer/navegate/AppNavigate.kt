f35f70c413f61552c5b97e35c6fe28e4f3ce7fec
Jordy <jord.mondaca@duocuc.cl>
2025-11-01 18:01:28 -0300

    Se definen las nuevas rutas en appNavigate.kt

--- app/src/main/java/com/example/levelup_gamer/navegate/AppNavigate.kt
+++ app/src/main/java/com/example/levelup_gamer/navegate/AppNavigate.kt
@@ -6,8 +6,8 @@ import androidx.navigation.NavHost
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.levelup_gamer.LoginScreen
 import com.example.levelup_gamer.ui.theme.Screen.BienvenidaScreen
+import com.example.levelup_gamer.ui.theme.Screen.LoginScreen
 import com.example.levelup_gamer.viewmodel.UsuarioViewModel
 
 
@@ -15,17 +15,42 @@ import com.example.levelup_gamer.viewmodel.UsuarioViewModel
 fun AppNavigate(){
     val navController = rememberNavController()
     val usuarioViewModel: UsuarioViewModel = viewModel()
+
     NavHost(
         navController = navController,
         startDestination = "login")
     {
         composable(route = "login") {
-            LoginScreen(usuarioViewModel,navController)
+            LoginScreen(navController,usuarioViewModel)
         }
         composable(route="bienvenida") {
             BienvenidaScreen(usuarioViewModel)
         }
+        composable(route = "bienvenida") {
+            // ... Llama a BienvenidaScreen
+        }
+        composable(route = "registro") {
+            // ... Llama a RegistroScreen
+        }
+        composable(route = "home") {
+            // ... Llama a la función Home(navController) que acabas de corregir
+        }
+        composable(route = "catalogo") {
+            // ... Llama a CatalogoScreen(navController)
+        }
+        composable(route = "perfil") {
+            // ... Llama a PerfilScreen(navController)
+        }
+        composable(route = "carrito") {
+            // ... Llama a CarritoScreen(navController)
+        }
+        composable(route = "about") {
+            // ... Llama a AboutScreen(navController)
+        }
+
     }
 
 }
+
+
 
