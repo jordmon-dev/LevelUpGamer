c5582530f01985562766f43fabe1c95fe69f96bc
Jordy <jord.mondaca@duocuc.cl>
2025-11-02 23:42:48 -0300

    Importación de pantallas About, Carrito y Producto

--- app/src/main/java/com/example/levelup_gamer/navegate/AppNavigate.kt
+++ app/src/main/java/com/example/levelup_gamer/navegate/AppNavigate.kt
@@ -2,55 +2,80 @@ package com.example.levelup_gamer.navegate
 
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavHost
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.levelup_gamer.ui.theme.Screen.BienvenidaScreen
-import com.example.levelup_gamer.ui.theme.Screen.LoginScreen
+// Importamos todas las pantallas del paquete correcto
+import com.example.levelup_gamer.ui.theme.Screen.*
 import com.example.levelup_gamer.viewmodel.UsuarioViewModel
+import com.example.levelup_gamer.viewmodel.ProductoViewModel
+import com.example.levelup_gamer.viewmodel.CarritoViewModel
+import com.example.levelup_gamer.viewmodel.AboutViewModel
 
 
 @Composable
 fun AppNavigate(){
     val navController = rememberNavController()
+
+    // Inyección de ViewModels al inicio para que persistan en toda la aplicación
     val usuarioViewModel: UsuarioViewModel = viewModel()
+    val productoViewModel: ProductoViewModel = viewModel()
+    val carritoViewModel: CarritoViewModel = viewModel()
+    val aboutViewModel: AboutViewModel = viewModel()
 
     NavHost(
         navController = navController,
-        startDestination = "login")
+        // La aplicación siempre comienza en la pantalla de Login
+        startDestination = "login"
+    )
     {
+        // 1. LOGIN
         composable(route = "login") {
-            LoginScreen(navController,usuarioViewModel)
+            // Pasamos navController y el ViewModel
+            LoginScreen(navController = navController, viewModel = usuarioViewModel)
         }
-        composable(route="bienvenida") {
-            BienvenidaScreen(usuarioViewModel)
-        }
-        composable(route = "bienvenida") {
-            // ... Llama a BienvenidaScreen
-        }
+
+        // 2. REGISTRO
         composable(route = "registro") {
-            // ... Llama a RegistroScreen
+            // Pasamos navController y el ViewModel
+            RegistroScreen(navController = navController, viewModel = usuarioViewModel)
         }
+
+        // 3. HOME (Destino después de un Login/Registro exitoso)
         composable(route = "home") {
-            // ... Llama a la función Home(navController) que acabas de corregir
+            Home(navController = navController)
         }
+
+        // 4. BIENVENIDA (Asumo que esta es la pantalla de Bienvenida de prueba)
+        composable(route="bienvenida") {
+            BienvenidaScreen(viewModel = usuarioViewModel)
+        }
+
+        // 5. CATÁLOGO
         composable(route = "catalogo") {
-            // ... Llama a CatalogoScreen(navController)
+            // Pasamos el ProductoViewModel
+            Catalogo(navController = navController, viewModel = productoViewModel)
         }
+
+        // 6. PERFIL
         composable(route = "perfil") {
-            // ... Llama a PerfilScreen(navController)
+            // Pasamos el UsuarioViewModel para ver/editar perfil
+            Perfil(navController = navController, viewModel = usuarioViewModel)
         }
-        /*composable(route = "carrito") {
-            // ... Llama a CarritoScreen(navController)
-        }*/
+
+        // 7. CARRITO
+        composable(route = "carrito") {
+            // Pasamos el CarritoViewModel
+            Carrito(navController = navController, viewModel = carritoViewModel)
+        }
+
+
+        // 8. ACERCA DE
         composable(route = "about") {
-            // ... Llama a AboutScreen(navController)
+            // Pasamos el AboutViewModel
+            About(navController = navController, viewModel = aboutViewModel)
         }
-
     }
-
 }
-
 
 
