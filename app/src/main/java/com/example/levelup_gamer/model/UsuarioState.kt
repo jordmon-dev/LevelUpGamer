5a7bee38b334be2b926909a11c1843eb9e7cd859
Jordy <jord.mondaca@duocuc.cl>
2025-11-21 02:11:45 -0300

    Se elimina la data class UsuarioErrores y todas las variables para dejarla como un archivo independiente, UsuarioErrores

--- app/src/main/java/com/example/levelup_gamer/model/UsuarioState.kt
+++ app/src/main/java/com/example/levelup_gamer/model/UsuarioState.kt
@@ -1,20 +1,14 @@
 package com.example.levelup_gamer.model
 
+
 data class UsuarioState(
     val nombre: String = "",
-    val correo: String = "",
-    val edad: String = "",
+    val email: String = "",
     val password: String = "",
-    val confirmPassword: String = "",
     val aceptarTerminos: Boolean = false,
+    val nivel: Int = 1,
+    val puntosLevelUp: Int = 0,
+    val fechaRegistro: String = "",
     val errores: UsuarioErrores = UsuarioErrores()
 )
 
-data class UsuarioErrores(
-    val nombre: String? = null,
-    val correo: String? = null,
-    val edad: String? = null,
-    val password: String? = null,
-    val confirmPassword: String? = null,
-    val aceptaTerminos: String? = null
-)
