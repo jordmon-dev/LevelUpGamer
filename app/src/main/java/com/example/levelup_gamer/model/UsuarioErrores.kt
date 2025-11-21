c7b45814ea32bde51d0e3fc9db9f130936f8bc28
Jordy <jord.mondaca@duocuc.cl>
2025-11-21 02:12:06 -0300

    Se crea el archivo UsuarioErrores

---
+++ app/src/main/java/com/example/levelup_gamer/model/UsuarioErrores.kt
@@ -0,0 +1,8 @@
+package com.example.levelup_gamer.model
+
+data class UsuarioErrores(
+    var nombre: String? = null,
+    var email: String? = null,
+    var password: String? = null,
+    var aceptaTerminos: String? = null
+)
