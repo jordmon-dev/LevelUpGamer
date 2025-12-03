d3b94debd9ccaf0102ff125a6229aa78911b3bb4
Elias Vicencio <el.vicencio@duocuc.cl>
2025-12-02 23:31:02 -0300

    Se agregaron dependencias Retrofit para implementacion de APIS externas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,6 +104,22 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
 
+    /*=============================Dependencias Retrofit2=====================================*/
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+    // ViewModel utilities for Compose
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.3")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.3")
+
+    // Activity Compose
+    implementation("androidx.activity:activity-compose:1.9.0")
+    /*==================================================================*/
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
