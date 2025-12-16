5309060e8d7b946cef670e664b8810e76eabaa58
Jordy <jord.mondaca@duocuc.cl>
2025-12-16 10:19:26 -0300

    Se añade archivo orden para ver ordenes en perfil de administrador

---
+++ app/src/main/java/com/example/levelup_gamer/model/Orden.kt
@@ -0,0 +1,13 @@
+package com.example.levelup_gamer.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Orden(
+    @SerializedName("id") val id: Long? = null,
+    @SerializedName("numeroOrden") val numeroOrden: Long,
+    @SerializedName("total") val total: Int,
+    @SerializedName("nombreCliente") val nombreCliente: String,
+    @SerializedName("emailCliente") val emailCliente: String,
+    // La fecha la pone el backend automáticamente, no es necesario enviarla
+    @SerializedName("fecha") val fecha: String? = null
+)
