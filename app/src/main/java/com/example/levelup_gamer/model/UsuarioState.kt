dcd054eeb427114805b0ca91f36c9e2d993933b5
Jordy <jord.mondaca@duocuc.cl>
2025-12-19 19:35:02 -0300

    Se agregan nuevos campos a UsuarioState.kt para que lapantalla de Perfil y Pago puedan leer la dirección, los apellidos y el rol.

--- app/src/main/java/com/example/levelup_gamer/model/UsuarioState.kt
+++ app/src/main/java/com/example/levelup_gamer/model/UsuarioState.kt
@@ -2,18 +2,27 @@ package com.example.levelup_gamer.model
 
 data class UsuarioState(
     val nombre: String = "",
+    val apellidos: String = "",
     val email: String = "",
     val password: String = "",
+
+    //Campos de lógica registro/UI
     val confirmPassword: String = "",
     val aceptarTerminos: Boolean = false,
+
+    // Datos de envío o CHECKOUT
+    val direcion: String = "",
+    val region: String = "",
+    val comuna: String = "",
 
     // Datos del Perfil
     val nivel: Int = 1,
     val puntosLevelUp: Int = 0,
     val fechaRegistro: String = "",
 
-
+    //Campos de seguridad (roles)
     val rol: String = "CLIENTE",
 
     val errores: UsuarioErrores = UsuarioErrores()
 )
+
