bea981ab5b0b684b0e795a0610ed39e001860c43
Jordy <jord.mondaca@duocuc.cl>
2025-12-19 23:42:31 -0300

    Variable de resumen de la compra añadida

--- app/src/main/java/com/example/levelup_gamer/model/Orden.kt
+++ app/src/main/java/com/example/levelup_gamer/model/Orden.kt
@@ -9,5 +9,7 @@ data class Orden(
     @SerializedName("nombreCliente") val nombreCliente: String,
     @SerializedName("emailCliente") val emailCliente: String,
     // La fecha la pone el backend automáticamente, no es necesario enviarla
-    @SerializedName("fecha") val fecha: String? = null
+    @SerializedName("fecha") val fecha: String? = null,
+    @SerializedName("resumenCompra") val resumenCompra: String =""
+
 )
